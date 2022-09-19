b84ca7d1016bfa90a6e41bfc560d0c01c3f7d37d
Sayed Haamid Tore <toreshcoding@gmail.com>
2022-09-19 12:17:19 -0400

    working on about section

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const navLinks = document.getElementsByClassName('nav-links')[0]
 toggleBtn.addEventListener('click', () => {
   navLinks.classList.toggle('active')
 })
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
